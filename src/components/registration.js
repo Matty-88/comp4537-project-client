e7f074cb6bbbca9e147193a1cb8d9fe479cf72ba
Matty-88 <m94wing@gmail.com>
2025-03-30 19:17:40 -0700

    fixed url

--- src/components/registration.js
+++ src/components/registration.js
@@ -29,7 +29,7 @@ const RegisterPage = () => {
         }
     
         try {
-            const response = await fetch(`${SERVER_RENDER}/add-user`, {
+            const response = await fetch(`${url}/add-user`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
